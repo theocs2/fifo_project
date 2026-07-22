a05b522f72cf686101e914808a5130189412e6c1
Theo Sullivan <theocs2@illinois.edu>
2026-07-22 15:07:57 -0500

    Update async_fifo.sv

--- async_fifo.sv
+++ async_fifo.sv
@@ -16,5 +16,73 @@ module async_fifo #(
     output logic [DATA_WIDTH-1:0] rdata,
     output logic                  empty
 );
+    localparam PTR_WIDTH = ADDR_WIDTH + 1;
+
+    //local pointers to both write and read domains (binary)
+    logic[PTR_WIDTH-1:0] r_ptr_bin, w_ptr_bin;
+
+    //gray local pointers
+    logic[PTR_WIDTH-1:0] r_ptr_gray, w_ptr_gray;
+
+    //synchronized gray pointers
+    logic [PTR_WIDTH-1:0] w_ptr_gray_sync;  // w_ptr_gray synced to clk_rd
+    logic [PTR_WIDTH-1:0] r_ptr_gray_sync;  // r_ptr_gray synced to clk_wr
+
+    logic [ADDR_WIDTH-1:0] w_addr, r_addr; // RAM adddresses
+    
+    bin2gray u_bin2gray_wr (
+        .bin(w_ptr_bin),
+        .gray(w_ptr_gray)
+    );
+
+    gray2bin u_gray2bin_rd (
+        .gray(r_ptr_gray),
+        .bin(r_ptr_bin)
+    );
+
+    // ** insert FF synchronizers here ** 
+
+
+
+
+
+    // Pointer Increment Logic
+
+    always_ff @(posedge wclk or negedge wrst_n) begin
+        if(!wrst_n)
+            w_ptr_bin <= '0;
+        else if (wen && !full) 
+            w_ptr_bin <= w_ptr_bin + 1;
+    end
+
+    always_ff @(posedge rclk or negedge rrst_n) begin
+        if(!rrst_n) 
+            r_ptr_bin <= '0;
+        else if (ren && !full)
+            r_ptr_bin <= r_ptr_bin + 1; //increment pointer
+    end
+
+
+
+    //Empty if all gray bits match, checked in READ domain
+    assign empty = w_ptr_gray_sync == r_ptr_gray;
+
+
+    //Full condition checked in WRITE domain
+    assign full = (w_ptr_gray[ADDR_WIDTH-1] != r_ptr_gray_sync[ADDR_WIDTH-1]) && 
+    (w_ptr_gray[ADDR_WIDTH-2:0] == r_ptr_gray_sync[ADDR_WIDTH-2:0]);
+
+
+    //memory instantiation
+
+    dual_port_ram bram(
+        .wclk(wclk),
+        .wen(wen),
+        .waddr(w_addr),
+        .wdata(wdata),
+        .rclk(rclk),
+        .raddr(r_addr),
+        .rdata(rdata)
+    );
 
 endmodule
