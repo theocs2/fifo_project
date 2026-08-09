0a32ec2c1a15db50565ccee308a24da1646dd2a7
Theo Sullivan <theocs2@illinois.edu>
2026-08-09 14:28:46 -0500

    Update sync2ff.sv

--- sync2ff.sv
+++ sync2ff.sv
@@ -3,8 +3,22 @@ module sync2ff #(
 ) (
     input  logic             clk,
     input  logic             rst_n,
-    input  logic [WIDTH-1:0] d,
-    output logic [WIDTH-1:0] q
+    input  logic [WIDTH-1:0] d1,
+    output logic [WIDTH-1:0] q2
 );
+
+    logic [WIDTH-1:0] q1;
+
+    always_ff @(posedge clk) begin
+        if(!rst_n) begin
+            q2 <= '0;
+            q1 <= '0;
+        end else begin
+            q1 <= d1;
+            q2 <= q1;
+        end
+    end
+
+
 
 endmodule
